0edf2a58a6185f099c51242d9ef80b74d3a6a458
Unknown <zoudingyun@foxmail.com>
2017-09-27 16:22:26 +0800

    自用v1.0

--- src/cn/joinv/Main.java
+++ src/cn/joinv/Main.java
@@ -21,23 +21,45 @@ public class Main {
         String choose = sc.next();
         if(choose.equals("1"))//DDNSClient
         {
-            String gitStr ="";
-            gitStr+="POSTcns.api.qcloud.com/v2/index.php?Action=RecordModify&Nonce=";
-            gitStr+=(int)(Math.random()*90000+10000);
-            gitStr+="&Region=&SecretId=AKIDMelgUBR7JKBqW6OimgUOCpGVkrnXqE7o&SignatureMethod=HmacSHA256&Timestamp=";
-            gitStr+=(long)System.currentTimeMillis()/1000;
-            gitStr+="&domain=gitdraw.cn&recordId=321515145&recordLine=默认&recordType=A&subDomain=test&value="+getIPbyWeb();
+            String myIP = "";
+            while(true) {
+                String tmpStr = getIP();
+                if(myIP.equals(tmpStr))
+                {
+                    System.out.println("IP未改变，30s后重新检测！");
+                }
+                else {
+                    System.out.println("IP改变！");
+                    myIP=tmpStr;
+                    String gitStr = "";
+                    gitStr += "POSTcns.api.qcloud.com/v2/index.php?";
+                    String gitStr1 ="Action=RecordModify&Nonce="+ (int) (Math.random() * 90000 + 10000);
+                    gitStr1 += "&Region=&SecretId=AKIDMelgUBR7JKBqW6OimgUOCpGVkrnXqE7o&SignatureMethod=HmacSHA256&Timestamp=";
+                    gitStr1 += (long) System.currentTimeMillis() / 1000;
+                    gitStr1 += "&domain=gitdraw.cn&recordId=321515145&recordLine=默认&recordType=A&subDomain=test&value=" + myIP;
 
-            System.out.println(gitStr);
+                    gitStr+=gitStr1;
 
-            String key = new sun.misc.BASE64Encoder().encode(HMACSHA256(gitStr.getBytes(),"uhJcWFn9OXVOCpIZebP6KOGyXzTbdZVW".getBytes()));
+                    System.out.println(gitStr);
 
+                    String key = new sun.misc.BASE64Encoder().encode(HMACSHA256(gitStr.getBytes(), "uhJcWFn9OXVOCpIZebP6KOGyXzTbdZVW".getBytes()));
 
-            try {
-                key = URLEncoder.encode(key, "UTF-8");
-                System.out.println(key);
-            } catch (UnsupportedEncodingException e) {
-                System.out.println(e.toString());
+                    try {
+                        key = URLEncoder.encode(key, "UTF-8");
+                        System.out.println(key);
+                        gitStr1+="&Signature="+key;
+                        String rt = sendPost("https://cns.api.qcloud.com/v2/index.php?",gitStr1);
+                        System.out.println(rt);
+
+                    } catch (UnsupportedEncodingException e) {
+                        System.out.println(e.toString());
+                    }
+                }
+                try {
+                    Thread.sleep(30000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
             }
         }
         else if (choose.equals("2"))//返回访问者的ip
@@ -125,6 +147,47 @@ public class Main {
         return "-1";
     }
 
+    public static String getIP()
+    {
+
+        ServerSocket ss;
+        Socket socket;
+        BufferedReader in;
+        PrintWriter out;
+        while (true) {
+            try {
+                socket = new Socket("joinv.cn", 10000);
+
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+
+                out.println("getIP");
+                out.flush();
+
+                //BufferedReader line = new BufferedReader(new InputStreamReader(System.in));
+                String line = in.readLine();
+                String myIP = line;
+                System.out.println("本机ip:"+myIP);
+
+                out.close();
+                in.close();
+
+                socket.close();
+
+
+                return myIP;
+
+            } catch (IOException e) {
+                System.out.println("fuck!connect failed!");
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+    }
+
 
 
 /*
@@ -159,4 +222,71 @@ public class Main {
         return hs.toString().toUpperCase();
     }
 
+
+
+
+
+    /**
+     * 向指定 URL 发送POST方法的请求
+     *
+     * @param url
+     *            发送请求的 URL
+     * @param param
+     *            请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
+     * @return 所代表远程资源的响应结果
+     */
+    public static String sendPost(String url, String param) {
+        PrintWriter out = null;
+        BufferedReader in = null;
+        String result = "";
+        try {
+            URL realUrl = new URL(url);
+            // 打开和URL之间的连接
+            URLConnection conn = realUrl.openConnection();
+            // 设置通用的请求属性
+            conn.setRequestProperty("accept", "*/*");
+            conn.setRequestProperty("connection", "Keep-Alive");
+            conn.setRequestProperty("user-agent",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            // 发送POST请求必须设置如下两行
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            // 获取URLConnection对象对应的输出流
+            out = new PrintWriter(conn.getOutputStream());
+            // 发送请求参数
+            out.print(param);
+            // flush输出流的缓冲
+            out.flush();
+            // 定义BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+            System.out.println("发送 POST 请求出现异常！"+e);
+            e.printStackTrace();
+        }
+        //使用finally块来关闭输出流、输入流
+        finally{
+            try{
+                if(out!=null){
+                    out.close();
+                }
+                if(in!=null){
+                    in.close();
+                }
+            }
+            catch(IOException ex){
+                ex.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    //=========================
 }
+
+
+
